c5664365388989ff6f1298226eecd6a347527080
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2023-11-24 23:41:33 +0700

    feat: add read csv feature

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import sys
-import pandas as pd
 import knn
 import naive_bayes
+import csv
 
-df_train = pd.read_csv(sys.argv[1])
-df_validation = pd.read_csv(sys.argv[2])
+
+def read_csv(file):
+    result = list()
+    with open(file) as csvfile:
+        # Read csv file
+        reader = csv.reader(csvfile)
+        # Ignore header
+        next(reader, None)
+        for row in reader:
+            result.append(row)
+
+    # Convert all elements into float
+    new_result = [[float(x) for x in y] for y in result]
+
+    # Convert all target values into integer
+    for row in new_result:
+        row[len(new_result[0])-1] = int(row[len(new_result[0])-1]) # Co
+    return new_result
+
+
+df_train = read_csv(sys.argv[1])
+df_validation = read_csv(sys.argv[2])
 
 if sys.argv[0] == "knn.py":
     # Neighbors: 5
@@ -13,3 +33,10 @@ elif sys.argv[0] == "naive_bayes.py":
     naive_bayes.naive_bayes(df_train, df_validation)
 else:
     print("Perintah tidak valid. Silakan coba lagi.")
+
+
+
+# df_train = read_csv("data_train.csv")
+# df_validation = read_csv("data_validation.csv")
+#
+# print(df_train)
