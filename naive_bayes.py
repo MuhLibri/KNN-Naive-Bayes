9cecd3cbd201147c3cbbca12e18281ed51c0ca9d
JerichoFletcher <jericho.russel17@gmail.com>
2023-11-25 15:07:01 +0700

    feat: naive bayes improvement 1
    Co-authored-by: kevinjohn01 <13521042@std.stei.itb.ac.id>

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,9 +1,12 @@
 import math
+import pandas as pd
 
 class _Statistics:
     def __init__(self, data: list[float]):
         self.mean = _mean(data)
         self.std = _std(data)
+        self.min = min(data)
+        self.max = max(data)
         self.len = len(data)
     
     def probability(self, x: float) -> float:
@@ -20,6 +23,9 @@ def _std(data: list[float]) -> float:
     u = _mean(data)
     var = sum([(x - u)**2 for x in data]) / float(len(data) - 1)
     return math.sqrt(var)
+
+def _corr(data_1: list[float], data_2: list[float]):
+    return pd.Series(data_1).corr(pd.Series(data_2))
 
 def _group_by_class(data: list[list[float]]) -> dict[float, list[list[float]]]:
     group: dict[float, list[list[float]]] = dict()
@@ -49,16 +55,16 @@ def _calculate_probabilities(class_statistics: dict[float, list[_Statistics]], r
     total_rows = sum([class_statistics[cls][0].len for cls in class_statistics])
     probs: dict[float, float] = dict()
 
-    for cls, stats in class_statistics.items():
-        probs[cls] = class_statistics[cls][0].len / float(total_rows)
+    for cls, class_stat in class_statistics.items():
+        probs[cls] = math.log((class_statistics[cls][0].len) / float(total_rows))
 
-        for col_idx in range(len(stats)):
-            probs[cls] *= stats[col_idx].probability(row[col_idx])
+        for col_idx in range(len(class_stat)):
+            probs[cls] += math.log(class_stat[col_idx].probability(row[col_idx]))
     
     return probs
 
 def _classify(class_probabilities: dict[float, float]) -> float:
-    curr_cls, curr_max_p = None, -1.
+    curr_cls, curr_max_p = None, float('-inf')
 
     for cls, prob in class_probabilities.items():
         if prob > curr_max_p:
@@ -67,7 +73,7 @@ def _classify(class_probabilities: dict[float, float]) -> float:
     
     return curr_cls
 
-def _test(class_statistics: dict[float, list[_Statistics]], test_data: list[list[float]]):
+def _test(class_statistics: dict[float, list[_Statistics]], test_data: list[list[float]]) -> tuple[dict[float, float], float]:
     hit: dict[float, tuple[int, int]] = dict()
     for row in test_data:
         class_probs = _calculate_probabilities(class_statistics, row)
@@ -82,15 +88,52 @@ def _test(class_statistics: dict[float, list[_Statistics]], test_data: list[list
         hit[correct] = (hit[correct][0], hit[correct][1] + 1)
     
     result: dict[float, float] = dict()
+    sum = 0.
     for cls, (correct, total) in hit.items():
         result[cls] = float(correct) / float(total)
+        sum += result[cls]
 
-    return result
+    return result, sum / float(len(result))
+
+def _remove_correlated_cols(data: list[list[float]], threshold: float = .55) -> list[list[float]]:
+    columns = [x for x in zip(*data)]
+    corr_mat = [[0. for _ in range(len(columns) - 1)] for _ in range(len(columns) - 1)]
+
+    # Construct a correlation matrix between data features
+    for i in range(len(columns) - 1):
+        for j in range(i):
+            corr_val = _corr(columns[i], columns[j])
+            corr_mat[i][j] = corr_val
+
+    # Mark features that correlate to another feature above a threshold
+    to_delete = list()
+    for i in range(len(columns) - 2, -1, -1):
+        for j in range(i - 1, -1, -1):
+            if corr_mat[i][j] >= threshold: to_delete.append(j)
+    
+    # Delete marked features
+    to_delete.sort(reverse=True)
+    for i in to_delete:
+        columns = columns[:i] + columns[i + 1:]
+    return [list(x) for x in zip(*columns)]
 
 def naive_bayes(train: list[list[float]], test: list[list[float]]):
+    cleaned_train = _remove_correlated_cols(train)
+    cleaned_test = _remove_correlated_cols(test)
+
+    print('Classification over raw data')
     class_stats = _statistics_class(train)
-    print(f'Training data accuracy: {_test(class_stats, train)}')
-    print(f'Validation data accuracy: {_test(class_stats, test)}')
+    res, avg = _test(class_stats, train)
+    print(f'Training data accuracy:\n{res}\nAverage: {avg}')
+    res, avg = _test(class_stats, test)
+    print(f'Validation data accuracy:\n{res}\nAverage: {avg}')
+
+    print('\nClassification over cleaned data')
+    cleaned_class_stats = _statistics_class(cleaned_train)
+    res, avg = _test(cleaned_class_stats, cleaned_train)
+    print(f'Training data accuracy:\n{res}\nAverage: {avg}')
+    res, avg = _test(cleaned_class_stats, cleaned_test)
+    print(f'Validation data accuracy:\n{res}\nAverage: {avg}')
 
 if __name__ == '__main__':
     import file
